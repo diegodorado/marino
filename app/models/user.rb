3839ffe58c941485e780483d5c5a966390ea9054
Diego Dorado <diegodorado@gmail.com>
2013-08-11 15:32:05 -0300

    configured sendgrid and confirmable user emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User
   field :confirmation_token,   :type => String
   field :confirmed_at,         :type => Time
   field :confirmation_sent_at, :type => Time
-  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
+  field :unconfirmed_email,    :type => String # Only if using reconfirmable
 
 
   ## Trackable
