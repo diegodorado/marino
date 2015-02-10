d9f3c8b98a681ae50d701ac28b8548530fe9e223
Diego Dorado <diegodorado@gmail.com>
2015-02-09 21:32:02 -0300

    minimum style changes

--- app/controllers/api/crops_controller.rb
+++ app/controllers/api/crops_controller.rb
@@ -8,6 +8,7 @@ class Api::CropsController < ApplicationController
   end
 
   def show
+    respond_with @crop
   end
 
   def create
@@ -33,32 +34,15 @@ class Api::CropsController < ApplicationController
   end
 
   def destroy
-    if @crop.destroy
-      render json: '', status: :ok
-    else
-      render json: '', status: :unprocessable_entity
+    begin
+      if @crop.destroy
+        render json: @crop, status: :ok
+      else
+        render json: '', status: :unprocessable_entity
+      end
+    rescue
+      render json: '', status: :error
     end
   end
-
-  def update_prices
-
-    if @crop.update_price(params[:month], params[:price], current_user)
-      render json: @crop #head :no_content
-    else
-      render json: @crop.errors, status: :unprocessable_entity
-    end
-
-  end
-
-
-  def get_price
-    @crop = Crop.find(params[:crop_id])
-    @crop.update_price(params[:month], '22.8', current_user)
-
-    render json: @crop.get_price(params[:month])
-  end
-
-
-
 
 end
