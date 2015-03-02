58f31082a1833b756db23b5a9fe132ffd42641a3
Diego Dorado <diegodorado@gmail.com>
2015-03-02 20:20:39 -0300

    resolves conflict with sprockets trying to compila .map and .gzip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ Marino::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile = [ Proc.new{ |path| !File.extname(path).in?(['.js', '.css', '.map', '.gzip', ''] ) }, /(?:\/|\\|\A)application\.(css|js)$/ ]
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
